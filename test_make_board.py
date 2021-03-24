318c6df2dff0160ce94623bc2950aea25037a22c
Sally Poon <sallypoon@hotmail.com>
2021-03-23 18:12:43 -0700

    COMPLETE unit tests for make_board testing coordinates are within each level

--- test_make_board.py
+++ test_make_board.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
-from game import make_board
+from game import make_board, \
+    MAX_MAP_X_LVL1, MAX_MAP_Y_LVL1, \
+    MAX_MAP_X_LVL2, MAX_MAP_Y_LVL2, \
+    MAX_MAP_X_LVL3, MAX_MAP_Y_LVL3
 
 
 class TestMakeBoard(TestCase):
@@ -28,5 +31,30 @@ class TestMakeBoard(TestCase):
                 for element in key:
                     self.assertIsInstance(element, expected_instance)
 
-    def test_make_board_lvl_1_includes_coordinates_in_lvl_1(self):
+    def test_make_board_coordinate_keys_have_string_values(self):
+        expected_instance = str
+        actual = make_board(2)
+        for key in actual.keys():
+            if key is tuple:
+                self.assertIsInstance(actual[key], expected_instance)
+
+    def test_make_board_lvl_1_includes_coordinates_defined_for_lvl_1(self):
         actual = make_board(1)
+        for key in actual.keys():
+            if key is tuple:
+                self.assertTrue(key[0] in range(MAX_MAP_X_LVL1()) and
+                                key[1] in range(MAX_MAP_Y_LVL1()))
+
+    def test_make_board_lvl_2_includes_coordinates_defined_for_lvl_2(self):
+        actual = make_board(2)
+        for key in actual.keys():
+            if key is tuple:
+                self.assertTrue(key[0] in range(MAX_MAP_X_LVL2()) and
+                                key[1] in range(MAX_MAP_Y_LVL2()))
+
+    def test_make_board_lvl_3_includes_coordinates_defined_for_lvl_3(self):
+        actual = make_board(3)
+        for key in actual.keys():
+            if key is tuple:
+                self.assertTrue(key[0] in range(MAX_MAP_X_LVL3()) and
+                                key[1] in range(MAX_MAP_Y_LVL3()))
