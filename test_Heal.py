c92ad82eae49c4901ef6dbce2fff1dd877674ed0
Sally Poon <sallypoon@hotmail.com>
2021-03-26 00:11:13 -0700

    DEBUG final unit test

--- test_Heal.py
+++ test_Heal.py
@@ -6,20 +6,20 @@ from game import heal
 
 class TestHeal(TestCase):
 
-    @patch('game.roll', result=3)
-    def test_heal_no_HP_change_if_at_max_health(self, mock_roll):
+    @patch('game.roll', return_value=3)
+    def test_heal_no_change_if_at_max_health(self, mock_roll):
         sample_character = {"HP": 20, "hit_dice": (1, 4), "max-HP": 20}
         heal(sample_character)
         self.assertEqual(sample_character["HP"], sample_character["HP"])
 
-    @patch('game.roll', result=4)
+    @patch('game.roll', return_value=4)
     def test_heal_does_not_heal_over_max_health(self, mock_roll):
         sample_character = {"HP": 17, "hit_dice": (1, 4), "max-HP": 20}
         expected_hp = 20
         heal(sample_character)
         self.assertEqual(expected_hp, sample_character["HP"])
 
-    @patch('game.roll', result=6)
+    @patch('game.roll', return_value=6)
     def test_heal_adds_rolled_hit_dice_to_HP(self, mock_roll):
         sample_character = {"HP": 2, "hit_dice": (1, 8), "max-HP": 20}
         expected_hp = 8
