be1c8e6a6017da36eca18b338179406213b118b7
Sally Poon <sallypoon@hotmail.com>
2021-03-26 22:24:23 -0700

    UPDATE print_map unit tests

--- unit_tests/test_print_map.py
+++ unit_tests/test_print_map.py
@@ -7,38 +7,52 @@ from game import print_map
 
 class TestPrintMap(TestCase):
 
+    @patch('random.choice', return_value="[  ]")
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_character_at_0_0_board_dimensions_1_1(self, mock_output):
+    def test_print_map_character_at_0_0_board_dimensions_1_1(self, mock_output, mock_choice):
         sample_character = {"x-location": 0, "y-location": 0}
         sample_board = {"max-x": 1, "max-y": 1}
-        expected = "[\033[36m웃\033[0m]\n"
+        expected = "\n(\033[36m웃\033[0m)\n"
         print_map(sample_character, sample_board)
         actual_output = mock_output.getvalue()
-        self.assertEqual(actual_output, expected)
+        self.assertEqual(expected, actual_output)
 
+    @patch('random.choice', return_value="[  ]")
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_character_at_middle_of_board_dimensions_3_3(self, mock_output):
+    def test_print_map_character_at_middle_of_board_dimensions_3_3(self, mock_output, mock_choice):
         sample_character = {"y-location": 1, "x-location": 1, "some-key": "ABC"}
         sample_board = {"max-x": 3, "max-y": 3, (0, 0): "anything here"}
-        expected = "[  ][  ][  ]\n[  ][\033[36m웃\033[0m][  ]\n[  ][  ][  ]\n"
+        expected = "\n[  ][  ][  ]\n[  ](\033[36m웃\033[0m)[  ]\n[  ][  ][  ]\n"
         print_map(sample_character, sample_board)
         actual_output = mock_output.getvalue()
-        self.assertEqual(actual_output, expected)
+        self.assertEqual(expected, actual_output)
 
+    @patch('random.choice', return_value="[  ]")
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_character_at_bottom_row_last_column_of_board_dimensions_4_4(self, mock_output):
+    def test_print_map_character_at_bottom_row_last_column_of_board_dimensions_4_4(self, mock_output, mock_choice):
         sample_character = {"x-location": 3, "some-key": "ABC", "y-location": 3}
         sample_board = {"max-y": 4, "pies": "they're tasty!", "max-x": 4, }
-        expected = "[  ][  ][  ][  ]\n[  ][  ][  ][  ]\n[  ][  ][  ][  ]\n[  ][  ][  ][\033[36m웃\033[0m]\n"
+        expected = "\n[  ][  ][  ][  ]\n[  ][  ][  ][  ]\n[  ][  ][  ][  ]\n[  ][  ][  ](\033[36m웃\033[0m)\n"
         print_map(sample_character, sample_board)
         actual_output = mock_output.getvalue()
-        self.assertEqual(actual_output, expected)
+        self.assertEqual(expected, actual_output)
 
+    @patch('random.choice', return_value="[  ]")
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_contains_character_in_room_representation(self, mock_output):
+    def test_print_map_contains_character_in_room_representation(self, mock_output, mock_choice):
         sample_character = {"x-location": 14, "y-location": 8}
         sample_board = {"max-x": 25, "max-y": 25, (0, 0): "anything here"}
-        expected_character_rep = "[\033[36m웃\033[0m]"
+        expected_character_rep = "(\033[36m웃\033[0m)"
         print_map(sample_character, sample_board)
         actual_output = mock_output.getvalue()
         self.assertIn(expected_character_rep, actual_output)
+
+    @patch('random.choice', return_value="[  ]")
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_map_contains_god_representation(self, mock_output, mock_choice):
+        sample_character = {"x-location": 5, "y-location": 9}
+        sample_board = {"max-x": 25, "max-y": 25, (0, 0): "anything here"}
+        expected_god_rep = "\033[31m( ☩ )\033[0m"
+        print_map(sample_character, sample_board)
+        actual_output = mock_output.getvalue()
+        self.assertIn(expected_god_rep, actual_output)
